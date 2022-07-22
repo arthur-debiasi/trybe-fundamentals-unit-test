13dee06f0c466151972348c3b5eafb9c2360f326
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-07-21 23:25:37 -0300

    feat: testes complementares em productDetails.spec.js

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -44,8 +44,13 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     expect(typeof productDetails('Pente de Cabelo', 'Tiara')[1]).toEqual('object');
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
     expect(productDetails('Desodorante', 'Sabonete')[0]).not.toEqual(productDetails('Desodorante', 'Sabonete')[1]);
+    expect(productDetails('Desodorante', 'Sabonete')[0]).not.toEqual(productDetails('Desodorante', 'Sabonete')[1]);
+    expect(productDetails('Aspirina', 'Tylenol')[0]).not.toEqual(productDetails('Aspirina', 'Tylenol')[1]);
+    expect(productDetails('Aspirina', 'Tylenol')[0]).not.toEqual(productDetails('Aspirina', 'Tylenol')[1]);
   });
     // Teste se os dois productIds terminam com 123.
-  expect(productDetails('Gel fixador', 'Espelho portátil')[0].details.productId).toEqual('Gel fixador123');
-  expect(productDetails('Colírio', 'Óculos Escuros')[1].details.productId).toEqual('Óculos Escuros123');
+    expect(productDetails('Gel fixador', 'Espelho portátil')[0].details.productId).toEqual('Gel fixador123');
+    expect(productDetails('Gel fixador', 'Espelho portátil')[1].details.productId).toEqual('Espelho portátil123');
+    expect(productDetails('Colírio', 'Óculos Escuros')[0].details.productId).toEqual('Colírio123');
+    expect(productDetails('Colírio', 'Óculos Escuros')[1].details.productId).toEqual('Óculos Escuros123');
 });
